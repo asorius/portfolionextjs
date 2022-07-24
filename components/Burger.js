1882b94367a359bbd24089962a6720624609ba38
asorius <18assdd@gmail.com>
2022-07-24 16:55:35 +0100

    burder animations

--- components/Burger.js
+++ components/Burger.js
@@ -1,10 +1,7 @@
 import React from 'react';
 export default function Burger({ active, toggler }) {
-  const activeStyles = active
-    ? 'translate-y-2 rotate-45 ease-in-out duration-300'
-    : ' rotate-0 ease-in-out duration-300';
   const burgerLineStyles = (custom) =>
-    `h-px w-full bg-neutral-800 dark:bg-neutral-50 ${custom}`;
+    `h-px w-full bg-neutral-800 dark:bg-neutral-50 duration-300 ease-in-out ${custom}`;
   const divStyles = `h-full w-8 flex flex-col justify-around relative z-40 cursor-pointer `;
   return (
     <div
@@ -15,21 +12,15 @@ export default function Burger({ active, toggler }) {
       }}>
       <div
         className={burgerLineStyles(
-          active
-            ? 'translate-y-2 rotate-45 ease-in-out duration-300 '
-            : ' rotate-0 ease-in-out duration-300'
+          active ? 'translate-y-2 rotate-45' : 'translate-y-0 rotate-0'
         )}></div>
       <div
         className={burgerLineStyles(
-          active
-            ? ' translate-x-full opacity-0 ease-in-out duration-300 '
-            : ' translate-x-0 ease-in-out duration-300'
+          active ? 'translate-x-full opacity-0' : 'translate-x-0 opacity-100'
         )}></div>
       <div
         className={burgerLineStyles(
-          active
-            ? ' -translate-y-3 -rotate-45  ease-in-out duration-300 '
-            : ' rotate-0  ease-in-out duration-300'
+          active ? '-translate-y-3 -rotate-45' : 'translate-y-0 rotate-0'
         )}></div>
     </div>
   );
